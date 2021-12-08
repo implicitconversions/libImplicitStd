a9f5ec10eaace1653d15478b9f4a04151a981f60
Jake Stine <jake.stine@gmail.com>
2021-12-08 12:07:40 -0800

    icystdlib: remove submodule, convert into a local copy.
    This submodule is shared with PS2 and xHD Tooling (debugger) but the intent is to merge these into this masticore repo in the near future. Therefore the submodule concept as now outlived its usefulness.

---
+++ icy-app-glue/icy_assert.h
@@ -0,0 +1,39 @@
+#pragma once
+
+// Placeholder for the icy-gist library's assertion interface.
+// These can interface to internal assert() macros, or invoke abort() directly, as shown.
+// It is recommended to discard it and provide macro redirection to your own preferred assertion
+// framework. Redirection looks like so:
+
+/*
+#include "my-assertions.h"
+
+#define dbg_check(cond, ...)     (my_assert( ## __VA_ARGS ))
+#define house_check(cond, ...)     (my_assert( ## __VA_ARGS ))
+
+#define dbg_abort(...)           (my_abort ( ## __VA_ARGS ))
+#define house_abort(...)           (my_abort ( ## __VA_ARGS ))
+*/
+
+#include <cstdlib>      // for abort()
+#include <cstdio>
+
+#define ICY_TRACE2(f,l) f "(" # l "): "
+#define ICY_TRACE1(f,l) ICY_TRACE2(f,l)
+#define ICY_TRACE       ICY_TRACE1(__FILE__, __LINE__)
+
+#if !defined(dbg_check)
+#	define dbg_check(cond, ...)		((cond) || (fprintf(stderr, ICY_TRACE "(assert failed) " # cond ": " __VA_ARGS__ ), abort(),0))
+#endif
+
+#if !defined(dbg_abort)
+#	define dbg_abort(...)		    (fprintf(stderr, ICY_TRACE "(abort) " __VA_ARGS__ ), abort(),0)
+#endif
+
+#if !defined(house_check)
+#	define house_check(cond, ...)		((cond) || (fprintf(stderr, ICY_TRACE "(assert failed) " # cond ": " __VA_ARGS__ ), abort(),0))
+#endif
+
+#if !defined(house_abort)
+#	define house_abort(...)		    (fprintf(stderr, ICY_TRACE "(abort) " __VA_ARGS__ ), abort(),0)
+#endif
---
+++ inc/posix_file.h
@@ -0,0 +1,86 @@
+#pragma once
+
+// Lightweight macro-based POSIX cross-compilation header
+
+
+#if PLATFORM_SCE    // Sony Playstation
+#	include "posix_file_sce.h"
+#endif
+
+#include <cstdint>
+
+// off_t is too poorly defined to be of use. Let's define our own based on intmax_t.
+using x_off_t = intmax_t;
+
+#if PLATFORM_MSW
+#	include <fcntl.h>
+#	include <io.h>
+#   include <cstdio>
+#   include <sys/stat.h>
+
+	// windows POSIX libs are lacking the fancy new pread() function. >_<
+	extern size_t _pread(int fd, void* dest, size_t count, x_off_t pos);
+
+	// Windows has this asinine non-standard notion of text mode POSIX files and, worse, makes the
+	// non-standard behavior the DEFAULT behavior.  What the bloody hell, Microsoft?  Your're drunk.
+	// Go Home.  --jstine
+
+#	define posix_open(fn,flags,mode)   _open(fn,(flags) | _O_BINARY, mode)
+#	define posix_read   _read
+#	define posix_pread  _pread
+#	define posix_write  _write
+#	define posix_close  _close
+#	define posix_lseek  _lseeki64
+#	define posix_unlink _unlink
+#	define O_DIRECT		(0)		// does not exist on windows
+#	define DEFFILEMODE  (_S_IREAD | _S_IWRITE)
+
+#elif PLATFORM_POSIX
+
+#	define posix_open   open
+#	define posix_read   read
+#	define posix_pread  pread
+#	define posix_write  write
+#	define posix_close  close
+#	define posix_lseek  lseek
+#	define posix_unlink unlink
+
+#endif
+
+#if !defined(posix_close)
+#	error Unsupported platform.
+#endif
+
+// Lightweight helper class for POSIX stat, just to put things in a little more friendly container.
+struct CStatInfo
+{
+	uint32_t    st_mode;
+	intmax_t    st_size;
+
+	time_t      time_accessed;
+	time_t      time_modified;
+	time_t      time_created;
+
+	bool IsFile     () const;
+	bool IsDir      () const;
+	bool Exists     () const;
+
+	bool operator==(const CStatInfo& r) const {
+		return (
+			(st_mode		== r.st_mode			) &&
+			(st_size		== r.st_size			) &&
+			(time_accessed	== r.time_accessed		) &&
+			(time_modified	== r.time_modified		) &&
+			(time_created	== r.time_created		)
+		);
+	}
+
+	bool operator!=(const CStatInfo& r) const {
+		return !this->operator==(r);
+	}
+};
+
+
+extern int				posix_link		(const char* existing_file, const char* link);
+extern CStatInfo		posix_fstat		(int fd);
+extern CStatInfo		posix_stat		(const char* fullpath);
