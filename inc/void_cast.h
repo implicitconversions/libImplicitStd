992cafc5468e938854d0415cc7954fcd4086217d
Jake Stine <jake.stine@implicitconversions.com>
2022-04-17 06:18:22 -0700

    icystdlib: void_cast and appSettings tech notes added.

---
+++ inc/void_cast.h
@@ -0,0 +1,31 @@
+#pragma once
+
+
+template <typename T> struct _priv_voidcast { using Type = void; }
+
+// Valid specializations for combinations of const and non-const.
+// Anything not in this list results in compilation failure.
+
+template <typename T> struct _priv_voidcast<T*>                        { using Type = void*; }
+template <typename T> struct _priv_voidcast<T const*>                  { using Type = void const*; }
+														        	   
+template <typename T> struct _priv_voidcast<T**>                       { using Type = void**; }
+template <typename T> struct _priv_voidcast<T  const**>                { using Type = void const**; }
+template <typename T> struct _priv_voidcast<T* const*>                 { using Type = void* const*; }
+template <typename T> struct _priv_voidcast<T  const* const*>          { using Type = void* const*; }
+																	   
+template <typename T> struct _priv_voidcast<T***>                      { using Type = void***; }
+template <typename T> struct _priv_voidcast<T   const***>              { using Type = void const***; }
+template <typename T> struct _priv_voidcast<T*  const**>               { using Type = void* const**; }
+template <typename T> struct _priv_voidcast<T** const*>                { using Type = void** const*; }
+template <typename T> struct _priv_voidcast<T   const*  const**>       { using Type = void const* const**; }
+template <typename T> struct _priv_voidcast<T*  const*  const*>        { using Type = void* const* const*; }
+template <typename T> struct _priv_voidcast<T   const** const*>        { using Type = void const** const*; }
+template <typename T> struct _priv_voidcast<T   const*  const* const*> { using Type = void const* const* const*; }
+
+// Cast a pointer to a void pointer, preserving strictness about constness and indirection
+template <typename T> __nodebug
+constexpr typename _priv_void_cast::_priv_voidcast<T>::Type void_cast(T ptr)
+{
+	return (typename _priv_void_cast::_priv_voidcast<T>::Type)ptr;
+}
