41bff450cdfc5ae64742c5735e2c19f8419843fa
Jake Stine <jake.stine@implicitconversions.com>
2022-05-02 17:30:31 -0700

    PS5: Enable GPU markers on House builds, and simplify marker idiom with GPU_MARKER_SCOPE()

--- inc/MacroUtil.h
+++ inc/MacroUtil.h
@@ -5,3 +5,5 @@
 #define _ICY_CONCAT_(l,r) l##r
 #define ICY_CONCAT(l,r) _ICY_CONCAT_(l,r)
 
+#define _IMPL_AUTO_ANON(line) auto ICY_CONCAT(_auto_anonymous_, line) =
+#define AUTO_ANON _IMPL_AUTO_ANON(__LINE__)
