45c8931f12913735c42c32c27a8dd81d67525fd0
Jake Stine <jake.stine@implicitconversions.com>
2022-05-07 06:54:57 -0700

    LinkSectionComdat: CLANG was still stripping data out of the executable
    Curses to good technology.

--- src/LinkSectionComdat.cpp
+++ src/LinkSectionComdat.cpp
@@ -1,15 +1,22 @@
 
+#include <cstdio>
+#include <posix_file.h>
 #include "LinkSectionComdat.h"
 
-__nodebug __noinline
+__noinline
 void ForceSymbolToBeLinked::_ForceSymbolToBeLinked(const void* p) {
 
-	// another suitable option is to call some opaque function in a DLL that's assured not to be optimizable.
-	// on Windows this could be SetLastError(). I am opting for std::atomic here since it avoids touching
-	// anything that actually could matter (like errno), and the compiler barrier ensures things can't be
-	// optimized away even if the compiler happens to know a whole lot about the implementation details of
-	// whatever fnction we hope might be opaque.
+	// OK, so we have a __noinline directive but clang optimizes it away regardless. It doesn't inline the
+	// function but it seems to inspect the function internals and optimize the codegen anyway?
 
-	static std::atomic<int> force_side_effects;
-	force_side_effects = (intptr_t((void*)p));
+	// doesn't work. Clang probably too smart and knows we never read the value back so it optimizes away.
+	//static volatile std::atomic<int> force_side_effects;
+	//force_side_effects = (intptr_t((void*)p));
+
+	// [jstine 2022] Either of these work. I'm preferring posix_write because write(-1) is always a no-op
+	//   while printf %.0s I'm less sure about. Note these only work because we're passing *p into an opaque
+	//   function, meaning the function is implemented wholly in a DLL or PRX that the compiler cannot see.
+
+	posix_write(-1, p, 0);
+	//printf("%.0s", p);
 }
