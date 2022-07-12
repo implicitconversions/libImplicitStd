b68b7d0362596fd7d31ab3b69142125f8f0ab582
Jake Stine <jake.stine@implicitconversions.com>
2022-07-12 11:55:18 -0700

    defer: added DeferFunc for use when anonymous lambda is a pita to debug

--- inc/defer.h
+++ inc/defer.h
@@ -39,12 +39,23 @@ public:
 	auto anon_defer_lambda_ ## lineno = [&]() { func; }; \
 	auto anon_defer_        ## lineno = _impl_anon_defer_t { anon_defer_lambda_ ## lineno }
 
+#define _deferFunc_expand_lineno_2(func, lineno) \
+	auto anon_defer_        ## lineno = _impl_anon_defer_t { func }
+
 #define _defer_expand_counter_1(func, lineno) _defer_expand_counter_2(func, lineno)
+#define _defer_expand_lineno_1(func, lineno) _defer_expand_counter_2(func, lineno)
+
 
 // defers a statement until the end of the current lexical scope. The parameter given must
 // be a valid statement. Variables or pointers to functions are not permissible.
-#define Defer(function_content)   _defer_expand_counter_1(function_content, __LINE__ )
+#define Defer(function_content)   _defer_expand_counter_1(function_content, __LINE__)
 
+// defers a function invocation or lambda. The function signature must be no args, eg. func().
+// intended use case is when an anonymous lambda defined by macro is too much of a pita to debug.
+#define DeferFunc(func) _defer_expand_lineno_1(func, __LINE__)
+
+
+// helpful for showing intent when using defer as a lightweight local-scope pointer manager.
 template<typename T>
 T* ReleaseDefer(T* (&ptr)) {
 	auto result = ptr;
