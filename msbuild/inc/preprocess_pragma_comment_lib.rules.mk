3885f5df6318bdf655aa93c4745a1f4d35784f97
Jake Stine <jake.stine@implicitconversions.com>
2024-02-19 17:12:06 -0800

    make: remove dependency on mkobjdir macro from pragma_comment_lib.mk

--- msbuild/inc/preprocess_pragma_comment_lib.rules.mk
+++ msbuild/inc/preprocess_pragma_comment_lib.rules.mk
@@ -8,9 +8,7 @@ m_pragma_comment_mydir := $(dir $(realpath -m $(lastword $(MAKEFILE_LIST))))
 
 $(TARGET_FULLPATH): LDFLAGS += $(shell cat $(PRAGMA_LIB_DEPS.LD) < /dev/null 2> /dev/null)
 $(OBJDIR)/%.pragma_lib: %.cpp $(INCREMENTAL_DEPS.CXX)
-:@  $(call mkobjdir)
 :@  $(CXX) -E $< $(all_includes.macro) $(CPPFLAGS) | $(m_pragma_comment_mydir)/pragma_comment_lib_list.sh $@
 
 $(OBJDIR)/%.pragma_lib: %.c $(INCREMENTAL_DEPS.C)
-:@  $(call mkobjdir)
 :@  $(CC) -E $< $(all_includes.macro) $(CPPFLAGS) | $(m_pragma_comment_mydir)/pragma_comment_lib_list.sh $@
