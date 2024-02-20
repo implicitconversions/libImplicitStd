2e7fc01dc62003d77beceb3b9d405ca8e9772dfe
Jake Stine <jake.stine@implicitconversions.com>
2024-02-19 16:32:31 -0800

    makefile: change link_switch -> pragma_lib

--- msbuild/inc/preprocess_pragma_comment_lib.rules.mk
+++ msbuild/inc/preprocess_pragma_comment_lib.rules.mk
@@ -1,16 +1,16 @@
 
 # Default Behavior: assume all OBJECTS should be parsed for #pragma comment(lib,) according to c/cpp extension rules.
-ifeq ($(LINK_SWITCH_DEPS.LD),)
-    LINK_SWITCH_DEPS.LD := $(OBJECTS:.o=.link_switch)
+ifeq ($(PRAGMA_LIB_DEPS.LD),)
+    PRAGMA_LIB_DEPS.LD := $(OBJECTS:.o=.pragma_lib)
 endif
 
 m_pragma_comment_mydir := $(dir $(realpath -m $(lastword $(MAKEFILE_LIST))))
 
-$(TARGET_FULLPATH): LDFLAGS += $(shell cat $(LINK_SWITCH_DEPS.LD) < /dev/null 2> /dev/null)
-$(OBJDIR)/%.link_switch: %.cpp $(INCREMENTAL_DEPS.CXX)
+$(TARGET_FULLPATH): LDFLAGS += $(shell cat $(PRAGMA_LIB_DEPS.LD) < /dev/null 2> /dev/null)
+$(OBJDIR)/%.pragma_lib: %.cpp $(INCREMENTAL_DEPS.CXX)
 :@  $(call mkobjdir)
 :@  $(CXX) -E $< $(all_includes.macro) $(CPPFLAGS) | $(m_pragma_comment_mydir)/pragma_comment_lib_list.sh $@
 
-$(OBJDIR)/%.link_switch: %.c $(INCREMENTAL_DEPS.C)
+$(OBJDIR)/%.pragma_lib: %.c $(INCREMENTAL_DEPS.C)
 :@  $(call mkobjdir)
 :@  $(CC) -E $< $(all_includes.macro) $(CPPFLAGS) | $(m_pragma_comment_mydir)/pragma_comment_lib_list.sh $@
